6f43a1e846a6793916c59903e2b05bd49eec02f7
yikwing <hubeieasy@gmail.com>
2024-02-07 11:26:45 +0800

    feat: add mumu

--- Surge/JS/mumu.js
+++ Surge/JS/mumu.js
@@ -9,6 +9,7 @@ let tmp = JSON.parse(body);
 console.log("==================\n\n" + body + "\n=================");
 
 tmp["data"]["current_device"]["trial_end_at"] = 1712246399;
+tmp["data"]["current_device"]["trial_status"] = 1;
 
 console.log("==================\n\n" + tmp + "\n=================");
 
