6842459600b43586d21b6f9f31f1bb153a83aafd
yikwing <yikwing@192.168.10.18>
2024-03-17 23:49:13 +0800

    feat: add aiqicha

--- Surge/JS/aiqicha.js
+++ Surge/JS/aiqicha.js
@@ -1,10 +1,53 @@
 const updateResponse = (response) => {
-  const parsedResponse = JSON.parse(response.body);
+  // const parsedResponse = JSON.parse(response.body);
 
-  parsedResponse.data.svip.status = 1;
-  parsedResponse.data.svip.endTime = "2024-12-31";
+  // parsedResponse.data.svip.status = 1;
+  // parsedResponse.data.svip.endTime = "2024-12-31";
 
-  return JSON.stringify(parsedResponse);
+  return JSON.stringify({
+    status: 0,
+    msg: "",
+    data: {
+      vip: 0,
+      svip: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+        residueDays: 0,
+      },
+      consume: 0,
+      icp: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+      },
+      time: "",
+      asset: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+      },
+      isRenewSuc: 0,
+      isHitOptimize: 0,
+      yuqing_junior: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+      },
+      comPackage: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+      },
+      yuqing_senior: {
+        status: 1,
+        startTime: "2023-11-06",
+        endTime: "2024-12-31",
+      },
+      signInStaus: 0,
+      isDisplayPop: 0,
+    },
+  });
 };
 
 const logResponse = (response) => {
