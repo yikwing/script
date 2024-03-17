f314c1416fcb5dcde38a9cae97c90c6f4935d891
yikwing <yikwing@192.168.10.18>
2024-03-17 13:36:31 +0800

    feat: add .vscode settings

--- Surge/JS/mumu.js
+++ Surge/JS/mumu.js
@@ -1,6 +1,8 @@
 const updateResponse = (response) => {
+  const nowTime = Date.parse(new Date()) / 1000;
+
   const parsedResponse = JSON.parse(response.body);
-  parsedResponse.data.current_device.trial_end_at = 1712246399;
+  parsedResponse.data.current_device.trial_end_at = nowTime + 31 * 3600 * 24;
   parsedResponse.data.current_device.trial_status = 1;
   return JSON.stringify(parsedResponse);
 };
